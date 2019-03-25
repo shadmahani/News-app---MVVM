198d68ce33f8509ace3e4c30f0d54adc063a8582
hossein shademani <hossein.sh93@gmail.com>
2019-03-25 18:48:30 +0430

    Initial Commit

---
+++ News app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News app
+//
+//  Created by hossein shademany on 3/25/19.
+//  Copyright © 2019 hossein shademany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
